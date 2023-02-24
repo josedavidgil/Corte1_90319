ab98ae13fc2fc3e885e6b3ad8d2bad8df0d1c12b
Jose David Gil <110634698+josedavidgil@users.noreply.github.com>
2023-02-23 20:20:51 -0500

    Add files via upload

---
+++ Taller N1/Primer Punto.py
@@ -0,0 +1,6 @@
+#Realice un programa donde se soliciten dos números al usuario, después se de como resultado el residuo y el cociente de la división entre <n1> y <n2>.
+n1=float(input("Ingrese el valor de N1: "))
+n2=float(input("Ingrese el valor de N2: "))
+div=n1/n2
+res=n1%n2
+print("El resultado de la división entre N1 y N2 es de:",div,"siendo su residuo",res)
