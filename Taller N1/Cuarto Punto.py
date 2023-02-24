c73d22e92a95966c959ee10891f082d33df76b91
Jose David Gil <110634698+josedavidgil@users.noreply.github.com>
2023-02-23 20:19:58 -0500

    Delete Taller N1 directory

--- Taller N1/Cuarto Punto.py
+++
@@ -1,5 +0,0 @@
-#Realice un programa que permita calcular el costo anual del consumo de combustible de un vehículo, si el usuario ingresa la distancia recorrida (Km) anual, el consumo de combustible (L / 100Km) anual y el costo promedio anual del combustible por litros recorridos ($ / L)
-d_rec=float(input("Ingrese la distancia recorrida anual en kilómetros (Km): "))
-c_anual=float(input("Ingrese el consumo de combustible anual (L/100km): "))
-c_prom=float(input("Ingrese el costo del combustible anual por litros recorridos ($/L): "))
-print("El costo anual del consumo de combustible del vehículo es de:$",(d_rec/ 100)*c_anual*c_prom)
--- Taller N1/Primer Punto.py
+++
@@ -1,4 +0,0 @@
-#Realice un programa donde se soliciten dos números al usuario, después se de como resultado el residuo y el cociente de la división entre <n1> y <n2>.
-n1=float(input("Ingrese el valor de N1: "))
-n2=float(input("Ingrese el valor de N2: "))
-print("El resultado de la división entre N1 y N2 es de:",(n1/n2),"siendo su residuo",(n1%n2))
